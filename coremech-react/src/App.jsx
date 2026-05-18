947627370d0082f0502fd6d6fe722ad50f915cae
isuka-6264 <destiny.vml@gmail.com>
2026-05-18 11:54:15 +0300

    feat: refactor registration and user list components with new styles

--- coremech-react/src/App.jsx
+++ coremech-react/src/App.jsx
@@ -1,10 +1,17 @@
-import ProfileCard from './components/ProfileCard'
+import { useState } from 'react'
 import './App.css'
+import ProfileCard from './components/ProfileCard'
+import UserList from './components/UserList'
+import RegisterForm from './components/RegisterForm'
 
 function App() {
+  const [refresh, setRefresh] = useState(0)
+
   return (
     <div>
       <ProfileCard />
+      <RegisterForm onRegister={() => setRefresh(r => r + 1)} />
+      <UserList refresh={refresh} />
     </div>
   )
 }
